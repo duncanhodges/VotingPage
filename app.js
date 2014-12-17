c9dd5ac3fad557252482d7a413aafde832406e80
Dunc <duncan2005@gmail.com>
2014-12-17 19:55:45 +0000

    Got some simple stuff working
    Now works well grabbing the results and punting them into a UL

--- app.js
+++ app.js
@@ -3,16 +3,29 @@ var express = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var connectCounter=0;
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
+app.get('/voter', function(req, res) {
+    res.sendFile(__dirname + "/voter.html");
+});
+
 io.on('connection', function(socket) {
-    socket.on('chat message', function(msg) {
-        io.emit('chat message', msg);
+
+    socket.on('gather',function(msg) {
+    	io.emit('gather');
     });
+
+    socket.on('my score', function(msg) {
+    	io.emit('push results',msg);
+    	console.log("received score of "+msg);
+    });
+        
 });
 
 http.listen(3000, function() {
